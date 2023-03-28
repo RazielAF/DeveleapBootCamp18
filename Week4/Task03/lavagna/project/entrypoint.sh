5dcc5a703a6322b23c6e4276f7bfbb30ce452543
Raziel <afandaev93@gmail.com>
2023-03-29 00:08:55 +0300

    Task lavagna almost finised

--- Week4/Task03/lavagna/project/entrypoint.sh
+++ Week4/Task03/lavagna/project/entrypoint.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
 
-echo "staring.."
-java -jr
+#!/bin/bash
+#connect to mysql server
+java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
+-Ddatasource.url="${DB_URL}" \
+-Ddatasource.username="${DB_USER}" \
+-Ddatasource.password="${DB_PASS}" \
+-Dspring.profiles.active="${SPRING_PROFILE}" \
+-jar target/lavagna-jetty-console.war
